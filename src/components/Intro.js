c044c35443e397d1b04945e9715bf47783613d62
sarvesh104 <sarveshdusane104@gmail.com>
2023-09-15 19:15:41 +0530

    main-content

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -23,3 +23,42 @@ const Intro = () => {
 }
 
 export default Intro;
+
+
+
+// import React from 'react';
+// import introi from "../images/intromob.png";
+
+// const Main = () => {
+//   return (
+//    <div id='bg-img'>
+//       <div class="container text-center" id='main'>
+//   <div class="row row-cols-2">
+//     <div class="col-6" id='main-1'><div class="container text-center">
+ 
+//   <div class="row">
+//     <div class="col-sm" id='intro-1'>—  INTRODUCING</div>
+//     </div>
+//     <div class="row">
+//     <div class="col-sm" id="intro-2"> Pass2Open</div>
+//     </div>
+//     <div class="row">
+      
+//     <div class="col-sm" id="intro-3" >What if your children had to look at an <br/> educational flashcard every time they  <br/>opened YouTube or Snapchat? Or <br/> perhaps answer a question to get into <br/> TikTok?</div>
+//   </div>
+// </div>
+      
+//     </div>
+//     <div class="col-6" id='main-2'>
+
+//     <img src={introi} class="img-fluid" alt="..."   />
+//     </div>
+    
+//   </div>
+// </div>
+// </div>
+    
+//   )
+// }
+
+// export default Main
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,3 +19,70 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+
+
+
+// import React from 'react';
+// import logo from "../images/passopen.png"
+
+// const Header = () => {
+//     return (
+//         <div class="container text-center" style={{marginTop:"20px",marginLeft:"100px"}}>
+//             <div class="row">
+//                 <div class="col-6" id='int-text1'><img src={logo} style={{marginTop: "10px",
+//       width: "155px", marginLeft: "79px"}} /></div>
+//                 <div class="col-sm-3" id='int-t-2'> <button  style={{marginTop:"10px", height: "45px" , padding:"2px 40px ", fontWeight:700}}type="button" class="btn btn-outline-secondary">Log in</button>
+// </div>
+//                 <div class="col-sm-3" id='int-t-3'><button  style={{  marginTop:"10px",  width: "80%", padding: "0px 20px", fontWeight:700}}type="button" class="btn btn-primary">Get App</button></div>
+//             </div>
+
+//         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //     <div class="container text-center"  style={{marginTop:"10px" ,
+        //     }} >
+        //   <div class="row row-cols-1 row-cols-sm-3   row-cols-md-3">
+        //     <div class="col"><img src={logo} style={{marginTop: "10px",
+        //     width: "155px"}} /></div>
+
+        //     {/* <div class="col" ></div> */}
+        //     <div class="col"> 
+
+        //   <button  style={{marginTop:"10px", height: "45px" , padding:"2px 40px "}}type="button" class="btn btn-outline-secondary">Secondary</button>
+        // </div>
+
+        //     <div class="col"><ul class="nav nav-pills nav-justified">
+        //   <li class="nav-item">
+        //     <a class="nav-link active" aria-current="page" href="#" style={{  marginTop:"10px",  width: "60%",
+        //     borderRadius: "4px"}}>Active</a>
+        //   </li>
+        // </ul>
+        // </div>
+        //   </div>
+        // </div>
+//     )
+// }
+
+// export default Header
